523db2fc1159dba9f4724aec3dd1b1cd9e0b55d1
ZeroRaptor <willow.dyson117@gmail.com>
2021-04-22 23:15:46 +0100

    added accuracy metrics

--- script.js
+++ script.js
@@ -9,6 +9,13 @@ $('#txtBlur').attr('draggable', false);
 
 var targStr = "Apparently, I need to think of some sentences to fill the space with :)";
 var pos = 0;
+// To keep counting after letters start being removed from the str
+var realPos = 0;
+var mistakes = 0;
+var seconds = 0;
+var miliseconds = 0;
+var clock;
+var accuracy = 100;
 $("#txtOut").text(targStr);
 
 $(document).keypress(function(e){
@@ -24,6 +31,7 @@ $(document).keypress(function(e){
     if($("#txtOut").text().split("")[pos] == e.key){
         $("#txtIn").text($("#txtIn").text() + e.key);
         pos++;
+        realPos++;
 
         // Move text along
         if(pos>13){
@@ -41,8 +49,43 @@ $(document).keypress(function(e){
         let regex = new RegExp(`.{${pos}}`);
         $("#txtOut").html(function (i,html){
             return html.replace(regex,'<span class="grey">$&</span>');
-            //return html.replace(/.{2}/,'<span class="grey">$&</span>');
         });
+
+        // Move Car
+        var increment = $(window).width() * 0.9;
+        increment -= $("#carImg").width();
+        increment /=  targStr.length;
+        $("#carImg").css({left:"+="+increment});
+
+        // Start Clock
+        if(realPos == 1){
+            clock = setInterval(function() {
+                miliseconds++;
+                if(miliseconds == 100){
+                    miliseconds = 0;
+                    seconds++;
+                }
+                $('#timer').text(seconds + "." + miliseconds.toString().padStart(2, "0") + "s");
+            }, 10);
+        }
+
+        // End Clock
+        if(realPos == targStr.length){
+            clearInterval(clock);
+        }
+
+    } else {
+        // Add a mistake
+        mistakes++;
+        $("#txtCont").css({borderColor:"red",color:"red"});
+        setTimeout(function(){
+            $("#txtCont").css({borderColor:"white",color:"white"});
+        },150);
     }
+
+    // Recalculate Accuracy
+    accuracy = (100 / (realPos + mistakes)) * realPos;
+    accuracy = Math.floor(accuracy)
+    $("#mistakes").text(accuracy + "%");
 
 });
